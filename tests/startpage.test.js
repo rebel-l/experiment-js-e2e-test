535e7b741896f94d6ac92fdfca0fc9e97e73c15a
Lars Gaubisch <lars.gaubisch@home24.de>
2017-07-26 19:23:49 +0200

    did a basic setup for jest with webdriver

---
+++ tests/startpage.test.js
@@ -0,0 +1,23 @@
+'use strict';
+const webdriver = require('selenium-webdriver');
+
+let browser;
+
+(function extendPath() {
+    let pathOfDrivers = process.cwd() + '/node_modules/.bin';
+    process.env['PATH'] = process.env['PATH'] + ':' + pathOfDrivers;
+})(); // should be part of a global application or application config
+
+beforeEach(() => {
+    browser = new webdriver.Builder()
+        .forBrowser('chrome')
+        .build();
+});
+
+afterAll(() => {
+    browser.close();
+});
+
+test('check start page', () => {
+    expect(1).toBe(1);
+});
